d3efb198bb8539b0a063b94a3f5e4b0d29889ec6
Gooba <tsiky@nexthope.net>
2021-09-15 17:17:52 +0300

    future

---
+++ org_chart_organization/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+	'name': "Organization Chart Department",
+	'summary': """Dynamic display of your Department Organization""",
+	'description': """Dynamic display of your Department Organization""",
+	'author': "SLife Organization, Amichia Fréjus Arnaud AKA",
+	'category': 'Human Resources',
+	'version': '2.0',
+	'license': 'AGPL-3',
+	'depends': ['hr','stock'],
+	'data': ['views/org_chart_views.xml'],
+	'images': [
+		'static/src/img/main_screenshot.png'
+	],
+	'qweb': [
+        "static/src/xml/org_chart_department.xml",
+    ],
+	'installable': True,
+	'application': True,
+	'auto_install': False,
+}
---
+++ org_chart_organization/models/models.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class OrgChartDepartment(models.Model):
+	_name = 'org.chart.department'
+
+	name = fields.Char("Org Chart Department")
+
+	@api.model
+	def get_department_data(self):
+		data = {
+			'name': 'Company',
+			'title': '',
+			'children': [],
+		}
+		departments = self.env['product.category'].search([('parent_id','=',False)])
+		for department in departments:
+			data['children'].append(self.get_children(department, 'middle-level'))
+
+		return {'values': data}
+
+	
+	@api.model
+	def change_node(self):
+		import pudb
+		pudb.set_trace()
+		pass
+
+
+	@api.model
+	def get_children(self, dep, style=False):
+		data = []
+		dep_data = {'name': dep.name}
+		childrens = self.env['product.category'].search([('parent_id','=',dep.id)])
+		for child in childrens:
+			sub_child = self.env['product.category'].search([('parent_id','=',child.id)])
+			next_style= self._get_style(style)
+			if not sub_child:
+				data.append({'name': child.name,  'className': next_style})
+			else:
+				data.append(self.get_children(child, next_style))
+
+		if childrens:
+			dep_data['children'] = data
+		if style:
+			dep_data['className'] = style
+
+		return dep_data
+
+
+	def _get_style(self, last_style):
+		if last_style == 'middle-level':
+			return 'product-dept'
+		if last_style == 'product-dept':
+			return 'rd-dept'
+		if last_style == 'rd-dept':
+			return 'pipeline1'
+		if last_style == 'pipeline1':
+			return 'frontend1'
+
+		return 'middle-level'
